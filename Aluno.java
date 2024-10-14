ceb7feab73b606081cf5525a26b5a64941f7e928
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-10-14 15:33:33 -0300

    Update and rename main.java to Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    // Getters and Setters
+}
+
